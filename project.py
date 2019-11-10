3617711d75fe48b31b890b7a11cb1c2cf6a09cf4
T0wn <eirijomine@hotmail.com>
2019-11-10 23:10:30 +0100

    kan oppgi helt egendefinert stablemønster. Ikke lagt til i gui enda

--- project.py
+++ project.py
@@ -45,20 +45,25 @@ def pick_new_box(box_length, box_width, box_height):
 def place_box(x_pos, y_pos, z_pos, box_height, space_between_boxes, target, patern):
     y_indent = getIndent(space_between_boxes, target)
     x_indent = 0
-    rotation = patern
+    
+    # brukes hvis det ønskes custom plasering/rotasjon av boxer
+    rotation = patern[0]
+    x_move = patern[1]
+    y_move = patern[2]
 
     if (rotation == 1):
-        robot.MoveJ( target * transl(x_pos + x_indent, y_pos + y_indent, z_pos - 300) )
-        robot.MoveL( target * transl(x_pos + x_indent, y_pos + y_indent, z_pos - 100) )
-        robot.MoveL( target * transl(x_pos, y_pos, z_pos - box_height) )
+        robot.MoveJ( target * transl(x_pos + y_move + x_indent, y_pos + x_move + y_indent, z_pos - box_height - 250) )
+        robot.MoveL( target * transl(x_pos + y_move + x_indent, y_pos + x_move + y_indent, z_pos - box_height - 50) )
+        robot.MoveL( target * transl(x_pos + y_move, y_pos + x_move, z_pos - box_height) )
+        tool.DetachAll()
+        robot.MoveL( target * transl(x_pos + y_move, y_pos + x_move, z_pos - box_height - 100) )
     elif (rotation == 2):
         moveTarget = (target * rotz(pi/2))
-        robot.MoveJ( moveTarget * transl(y_pos + y_indent, -(x_pos + x_indent), z_pos - 300) )
-        robot.MoveL( moveTarget * transl(y_pos + y_indent, -(x_pos + x_indent), z_pos - 100) )
-        robot.MoveL( moveTarget * transl(y_pos, -x_pos, z_pos - box_height) )
-
-    tool.DetachAll()
-    robot.MoveL( target * transl(x_pos, y_pos, z_pos - 100) )
+        robot.MoveJ( moveTarget * transl(y_pos + x_move + y_indent, -(x_pos + y_move + x_indent), z_pos - box_height - 250) )
+        robot.MoveL( moveTarget * transl(y_pos + x_move + y_indent, -(x_pos + y_move + x_indent), z_pos - box_height - 50) )
+        robot.MoveL( moveTarget * transl(y_pos + x_move, -(x_pos + y_move), z_pos - box_height) )
+        tool.DetachAll()
+        robot.MoveL( moveTarget * transl(y_pos + x_move, -(x_pos + y_move), z_pos - box_height - 50) )
 
 
 def getIndent(space_between_boxes, target):
@@ -95,8 +100,7 @@ def box_per_direction():
 def calcheight(boxes_per_pallet, x, y):
     return math.ceil( boxes_per_pallet / (x * y) )
 
-
-def palletize(box_object, targetnr, boxes_in_x_dir, boxes_in_y_dir, boxes_in_z_dir, space_between_boxes, layer_pattern):
+def palletize(box_object, targetnr, boxes_in_x_dir, boxes_in_y_dir, boxes_in_z_dir, space_between_boxes, layer_pattern = None):
     box = sim.Item(box_object.name)
     box_length = box_object.length
     box_width = box_object.width
@@ -108,11 +112,19 @@ def palletize(box_object, targetnr, boxes_in_x_dir, boxes_in_y_dir, boxes_in_z_d
     y_max = boxes_in_y_dir
     z_max = boxes_in_z_dir
 
+    # genererer default pattern, hvis et custom pattern ikke er oppgitt
+    if (layer_pattern == None):
+        layer_pattern = []
+        for y in range(0, y_max):
+            layer_pattern.append([])
+            for x in range(0, x_max):
+                layer_pattern[y].append( (1, 0, 0) )
+
     for z in range(0, z_max):
         for y in range(0, y_max):
             for x in range(0, x_max):
 
-                placeNr = layer_pattern[y][x]
+                placeNr = layer_pattern[y][x][0]
 
                 positions = get_pos_of_box(x, y, z, x_max, y_max, box_length, box_width, box_height, space_between_boxes, target)
                 x_pos = positions[0]
@@ -122,14 +134,18 @@ def palletize(box_object, targetnr, boxes_in_x_dir, boxes_in_y_dir, boxes_in_z_d
                 if (placeNr > 0):
                     copy_new_box(box, box_height)
                     pick_new_box(box_length, box_width, box_height)
-                    place_box(x_pos, y_pos, z_pos, box_height, space_between_boxes, target, placeNr)
+                    place_box(x_pos, y_pos, z_pos, box_height, space_between_boxes, target, layer_pattern[y][x])
+
+    robot.MoveJ(home)
 
 
 
 
-testArray = [[2, 0, 2],
-            [1, 1, 1],
-            [2, 0, 2]
+
+testArray = [
+    [(2, -30, 15), (0, 0, 0), (2, -30, -15)],
+    [(1, 0, 0), (1, 0, 0), (1, 0, 0)],
+    [(2, 30, 15), (0, 0, 0), (2, 30, -15)]
 ]
 
 def getBoxes():
@@ -140,4 +156,4 @@ def getBoxes():
     return boxes
 
 if __name__ == "__main__":
-    palletize(getBoxes()[1], 1, 3, 3, 1, 40, testArray)
+    palletize(getBoxes()[1], 1, 3, 3, 1, 20, layer_pattern = testArray)
