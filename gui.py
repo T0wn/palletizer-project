2d037b95206df1a866e154b640deaec3eb2a7291
SSoppen <soppenfedg@gmail.com>
2019-12-02 16:16:02 +0100

    Fikset GUI

--- gui.py
+++ gui.py
@@ -43,9 +43,10 @@ class MainFrame:
         self.inputArray.append(newInput(master, "Boxes in x direction on layer:", 1, 6))
         self.inputArray.append(newInput(master, "Boxes in y direction on layer:", 1, 7))
         self.inputArray.append(newInput(master, "Space between boxes:", 1, 8))
+
         self.label = tk.Label(master, text="X/Y indent is only used on Custom target")
         self.label.grid(column=1, row=11, padx=2, pady=2)
-        self.inputArray = []
+
         self.inputArray.append(newInput(master, "Indentation in X", 1, 12))
         self.inputArray.append(newInput(master, "Indentation in Y:", 1, 13))
 
